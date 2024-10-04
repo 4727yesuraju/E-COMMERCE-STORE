5d3be25349a1a38e638638923e3fa6b1fdb2bb8e
4727yesuraju <94819056+4727yesuraju@users.noreply.github.com>
2024-10-05 04:59:12 +0530

    coupon model is created

--- api/server.js
+++ api/server.js
@@ -6,6 +6,7 @@ import cookieParser from 'cookie-parser'
 import authRoutes from './routes/auth.route.js';
 import productRoutes from './routes/product.route.js';
 import cartRoutes from './routes/cart.route.js';
+import couponRoutes from './routes/coupon.route.js';
 
 import { connectToDB } from './lib/db.js';
 config();
@@ -26,6 +27,7 @@ app.use(cookieParser())
 app.use("/api/auth",authRoutes);
 app.use("/api/product",productRoutes);
 app.use("/api/cart",cartRoutes);
+app.use("/api/coupon",couponRoutes);
 
 
 app.listen(PORT,()=>{
